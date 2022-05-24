e79f42f40331b1c1577170a0dfb82c82015571aa
John <88310061+yeh-john@users.noreply.github.com>
2022-05-24 10:46:43 -0400

    Add files via upload

---
+++ linux/bluetooth.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Start setup bluetooth on linux"
+echo ""
+echo "Are you running this program using sudo [y/n]"
+read start
+
+if [ $start = "y" ]
+then
+	sudo apt-get install blueman
+	sleep 1
+	sudo service bluetooth start
+	sleep 1
+	sudo systemctl enable bluetooth.service
+	sleep 1
+	sudo systemctl start bluetooth.service
+	sleep 1
+	echo ""
+	echo ""
+	# Ask do reboot
+	echo "Do you want to reboot? [y/n]"
+	read Askreboot
+	if [ $Askreboot = "y" ]
+	then
+		reboot
+	else
+		echo "Done...."
+	fi
+else
+	echo "Done....."
+fi
+
