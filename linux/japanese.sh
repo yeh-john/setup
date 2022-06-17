88c874c850a04d89f28e64a6128e9ac790f41ca2
John <88310061+yeh-john@users.noreply.github.com>
2022-06-17 03:46:38 -0400

    Update japanese.sh

--- linux/japanese.sh
+++ linux/japanese.sh
@@ -3,7 +3,7 @@ echo "Start setup Japanese on linux"
 echo ""
 echo "Are you running this program using sudo? [y/n]"
 read start
-if [ $start = "start" ]
+if [ $start = "start" ] || [ $start = "Y" ];
 then
    sudo apt-get install ibus-anthy
    sleep 2
@@ -13,7 +13,7 @@ then
    # Ask reboot
    echo "Do you want to reboot? [y/n]"
    read askReboot
-   if [ $askReboot = "y" ]
+   if [ $askReboot = "y" ] || [ $askReboot = "Y" ];
    then
       sudo reboot
    else
