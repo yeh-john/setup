35c89b010fb8e079917e57c8c05a839dc512c079
John <88310061+yeh-john@users.noreply.github.com>
2022-10-05 17:08:49 +0800

    Create coreminer-rp.sh

---
+++ linux/coreminer-rp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Start setup coreminer on linux"
+echo ""
+echo "Don't run this command using sudo"
+echo "Are you ready to run this command? [y/n]"
+read start
+if [ $start = "y" ] || [ $start = "Y" ];
+then
+   mkdir coreminer && cd coreminer
+   sleep 2
+   wget https://github.com/catchthatrabbit/coreminer/releases/download/v0.19.71/coreminer-linux-arm64.tar.gz
+   sleep 2
+   tar -xzvf ./coreminer-linux-arm64.tar.gz
+   sleep 2
+   rm -rf ./coreminer-linux-arm64.tar.gz
+   sleep 2
+   echo "Addr: cb702f743c65bc49cefd3e4ec2626c2bd1921d38c58c, Server: as.catchthatrabbit.com, Port: 8008" > info.txt
+   sleep 2
+   echo ""
+   echo "---------------------"
+   echo ""
+   # Ask reboot
+   echo "Do you want to reboot? [y/n]"
+   read askReboot
+   if [ $askReboot = "y" ] || [ $askReboot = "Y" ];
+   then
+      sudo reboot
+   else
+      echo "Done...."
+   fi
+else
+   echo "Done...."
+fi
